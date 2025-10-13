7cd234862e370289e03dfa20c718236f3f3dd959
Jas <73720805+JasmineLIE@users.noreply.github.com>
2025-10-12 20:28:37 -0400

    jasmine's sick and twisted calendar button

--- Lab3/app/src/main/java/com/example/lab3/AddNewItemActivity.java
+++ Lab3/app/src/main/java/com/example/lab3/AddNewItemActivity.java
@@ -1,11 +1,15 @@
 package com.example.lab3;
 
+import android.app.AlertDialog;
+import android.app.DatePickerDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
@@ -14,8 +18,12 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.Calendar;
+
 public class AddNewItemActivity extends AppCompatActivity {
 
+    private DatePickerDialog datePickerDialog;
+    private Button dateButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,14 +34,54 @@ public class AddNewItemActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        initDatePicker();
+        dateButton = findViewById(R.id.datePickerButton);
+        dateButton.setText(getTodaysDate());
+
+
     }
+
+    private String getTodaysDate() {
+        Calendar cal = Calendar.getInstance();
+        int year = cal.get(Calendar.YEAR);
+        int month = cal.get(Calendar.MONTH);
+        month++;
+        int day = cal.get(Calendar.DAY_OF_MONTH);
+
+        return makeDateString(day, month, year);
+    }
+    private void initDatePicker() {
+        DatePickerDialog.OnDateSetListener dateSetListener = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int day) {
+                month = month+1;
+                String date = makeDateString(day, month, year);
+                dateButton.setText(date);
+
+            }
+        };
+
+        Calendar cal = Calendar.getInstance();
+        int year = cal.get(Calendar.YEAR);
+        int month = cal.get(Calendar.MONTH);
+        int day = cal.get(Calendar.DAY_OF_MONTH);
+
+        int style = AlertDialog.THEME_HOLO_LIGHT;
+        datePickerDialog = new DatePickerDialog(this, style, dateSetListener, year, month, day);
+    }
+
+    private String makeDateString(int day, int month, int year) {
+        return month + " " + day + " " + year;
+    }
+
 
     public void onAddPressed(View view){
         EditText nameField = findViewById(R.id.nameField);
         EditText descriptionField = findViewById(R.id.descriptionField);
-        EditText deadlineField = findViewById(R.id.deadlineField);
+        //EditText deadlineField = findViewById(R.id.deadlineField);
 
-        TodoItem td = new TodoItem(nameField.getText().toString(),descriptionField.getText().toString(),deadlineField.getText().toString());
+        //TodoItem td = new TodoItem(nameField.getText().toString(),descriptionField.getText().toString(),deadlineField.getText().toString());
 
         SharedPreferences sharedPreferences = getSharedPreferences("Todos", MODE_PRIVATE);
 
@@ -43,7 +91,7 @@ public class AddNewItemActivity extends AppCompatActivity {
 
         editor.putString("name" + sharedPreferences.getInt("count",0), nameField.getText().toString());
         editor.putString("desc" + sharedPreferences.getInt("count",0), descriptionField.getText().toString());
-        editor.putString("dead" + sharedPreferences.getInt("count",0), deadlineField.getText().toString());
+        //editor.putString("dead" + sharedPreferences.getInt("count",0), deadlineField.getText().toString());
         editor.apply();
 
         Log.d("MainActivity","did it");
@@ -65,5 +113,9 @@ public class AddNewItemActivity extends AppCompatActivity {
         for (int i = 0; i < count; i++) {
             Log.d("MainActivity",sharedPreferences.getString("name"+i, ""));
         }
+    }
+
+    public void openDatePicker(View view) {
+        datePickerDialog.show();
     }
 }
