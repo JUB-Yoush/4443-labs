319a9eec3364c9d2c363f85d8f80bf794e4f4bc9
JUB_Yoush <yoush1337@gmail.com>
2025-10-29 18:33:54 -0400

    camera view

--- Lab4/app/src/main/java/com/example/lab4/MainActivity.java
+++ Lab4/app/src/main/java/com/example/lab4/MainActivity.java
@@ -1,9 +1,15 @@
 package com.example.lab4;
 
+import android.Manifest;
+import android.app.ComponentCaller;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
@@ -13,7 +19,11 @@ import androidx.activity.result.ActivityResult;
 import androidx.activity.result.ActivityResultCallback;
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
@@ -21,6 +31,7 @@ import androidx.core.view.WindowInsetsCompat;
 public class MainActivity extends AppCompatActivity {
 
     Button galleryBtn;
+Button photoBtn;
     ImageView profilePicture;
 
     ActivityResultLauncher<Intent> resultLauncher;
@@ -37,12 +48,26 @@ public class MainActivity extends AppCompatActivity {
         });
 
         galleryBtn = findViewById(R.id.galleryBtn);
+        photoBtn = findViewById(R.id.photoBtn);
         profilePicture = findViewById(R.id.profilePicture);
 
         registerResult();
 
         galleryBtn.setOnClickListener(view -> pickImage());
+
+        // request for camera
+        if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(MainActivity.this, new String[]{
+                   Manifest.permission.CAMERA
+            }, 100);
+        }
+
+        photoBtn.setOnClickListener(v -> {
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(intent,100);
+    });
     }
+
 
     private void pickImage(){
        Intent intent = new Intent(MediaStore.ACTION_PICK_IMAGES);
@@ -64,6 +89,14 @@ public class MainActivity extends AppCompatActivity {
                     }
                 }
         );
+    }
 
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data, @NonNull ComponentCaller caller) {
+        super.onActivityResult(requestCode, resultCode, data, caller);
+        if (requestCode == 100){
+            Bitmap bitmap = (Bitmap) data.getExtras().get("data");
+            profilePicture.setImageBitmap(bitmap);
+        }
     }
 }
