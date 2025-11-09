ecb77b3de1207a5065340a7917b519bf26c9f031
Lusa <jasminely428@gmail.com>
2025-11-09 14:31:48 -0500

    List view

--- lab_5/test/widget_test.dart
+++ lab_5/test/widget_test.dart
@@ -11,6 +11,9 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:lab_5/main.dart';
 
 void main() {
+  /**
+   * 
+
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
     await tester.pumpWidget(const MyApp());
@@ -27,4 +30,5 @@ void main() {
     expect(find.text('0'), findsNothing);
     expect(find.text('1'), findsOneWidget);
   });
+     */
 }
